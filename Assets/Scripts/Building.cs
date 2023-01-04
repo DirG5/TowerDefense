5fdc5e8025ef6cd65ac427cfcc801ee407d0f776
DirG5 <kupa.yu.v@gmail.com>
2023-01-04 18:08:43 +0200

    Initial commit

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private Vector2 buildingSize;
+    [SerializeField] private Renderer renderer;
+
+    public Vector2 BuildingSize => buildingSize;
+
+    public void SetColor(bool isAvailableToBuild)
+    {
+        if (isAvailableToBuild)
+            renderer.material.color = Color.green;
+        else
+            renderer.material.color = Color.red;
+    }
+
+    public void ResetColor()
+    {
+        renderer.material.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    private static GridController _instance;
+
+    public static GridController Instance => _instance;
+
+    private Building[,] _grid;
+
+    public Building[,] Grid
+    {
+        get => _grid;
+        set => _grid = value;
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(gameObject);
+        else
+            _instance = this;
+    }
+}
